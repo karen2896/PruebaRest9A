bfb14123861903c05cd808b94e63ca8413ec1914
Karen <2136000186@utna.edu.mx>
2025-06-26 19:35:11 -0600

    Practica de conexion en mongoose a mongodb

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require('./App/app')
 const config = require('./App/config/configuracion')
+const conexion =require('./App/config/conexion')
+conexion.connect()
 
 app.listen(config.PORT,()=>{
     console.log(`Aplicacion corriendo en le puerto ${config.PORT}`);
